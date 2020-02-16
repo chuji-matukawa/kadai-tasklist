dafc8b4be75ee2679704c4f82a4e7c611d1174de
chuji.matukawa <webeng0405onepan@icloud.com>
2020-02-16 02:06:18 +0000

    crud column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,7 @@
         @include('commons.navbar')
         
         <div class="container">
-            
-        @if (count($errors) > 0)
-            <ul class="alert alert-danger" role="alert">
-            @foreach ($errors->all() as $error)
-            <li class="ml-4">{{ $error }}</li>
-            @endforeach
-            </ul>
-        @endif
+            @include('commons.error_messages')
 
             @yield('content')
         </div>
